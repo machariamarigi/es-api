e4c22abfc36870e67c12bbd8ff6a26d8c1250a62
machariamarigi <machariamarigi@gmail.com>
2019-11-30 23:45:05 +0300

    add products provider

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+
 import { ProductsService } from './products.service';
 import { ProductsController } from './products.controller';
+import { ProductsProviders } from './products.provider';
 
 @Module({
-  providers: [ProductsService],
-  controllers: [ProductsController]
+  providers: [ProductsService, ...ProductsProviders],
+  controllers: [ProductsController],
 })
 export class ProductsModule {}
