6b16beb2de90c876b5bed286072c3607d8436caf
machariamarigi <machariamarigi@gmail.com>
2019-11-30 23:47:16 +0300

    finish setup

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -3,8 +3,10 @@ import { Module } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { ProductsController } from './products.controller';
 import { ProductsProviders } from './products.provider';
+import { DatabaseModule } from '../database/database.module';
 
 @Module({
+  imports: [DatabaseModule],
   providers: [ProductsService, ...ProductsProviders],
   controllers: [ProductsController],
 })
