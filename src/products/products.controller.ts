522a0e20b9ccf2ba41c73302751c0af09f8fd89e
machariamarigi <machariamarigi@gmail.com>
2019-11-30 21:36:59 +0300

    get all products in the products controller

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import * as _ from 'lodash';
+
+import { ProductsService } from './products.service';
 
 @Controller('products')
-export class ProductsController {}
+export class ProductsController {
+  constructor(private readonly productService: ProductsService) {}
+
+  @Get()
+  getProducts(): any {
+    return this.productService.findAll().then(
+      products => {
+        return products.map(product => _.omit(product.toJSON(), ['sales']));
+      },
+    );
+  }
+}
